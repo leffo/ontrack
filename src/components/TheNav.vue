ff5cdf4dfd97f2d6ee19aa907c35bfc5de10dbb0
Alex Yakovlev <leffalt@gmail.com>
2024-01-08 16:57:39 +0700

    Module with functions

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -11,7 +11,7 @@ const emit = defineEmits(['navigate'])
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <NavItem v-for="icon, page in NAV_ITEMS"
+      <NavItem v-for="(icon, page) in NAV_ITEMS"
                :key="page"
                :href="`#${page}`"
                :class="{ 'bg-gray-200 pointer-events-none': page === currentPage }"
