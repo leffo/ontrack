fac3392968625346ac7b0ea00df5ab086f8b6302
Alex Yakovlev <leffalt@gmail.com>
2024-01-08 14:07:34 +0700

    initial commit ontrack

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TheHeader from '@/components/TheHeader.vue'
+import TheNav from '@/components/TheNav.vue'
+
+</script>
+
+<template>
+  <TheHeader />
+  <main class="flex flex-grow flex-col"></main>
+  <TheNav />
+</template>
+
+<style scoped>
+</style>
